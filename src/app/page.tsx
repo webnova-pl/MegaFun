3afd30d0f421814d8baf22fa77e0c30441424fc5
Dawid Slowik <dawidslowik379@gmail.com>
2025-03-09 18:47:02 +0100

    update before publish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import AboutSection from "@/sections/AboutSection/AboutSection";
 import OfferSection from "@/sections/OfferSection/OfferSection";
 import RentSection from "@/sections/RentSection/RentSection";
 import HeroSection from "../sections/HeroSection/HeroSection";
-import DeliverySection from "@/sections/DeliverySection/DeliverySection";
 export default function Home() {
 	return (
 		<main className="">
 			<HeroSection />
 			<AboutSection />
-			{/* <DeliverySection /> */}
 			<OfferSection />
 			<RentSection />
 		</main>
