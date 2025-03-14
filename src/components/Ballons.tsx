098928b378cf27d7c8801161183097ab67ce7ebc
Dawid Slowik <dawidslowik379@gmail.com>
2025-03-14 12:28:34 +0100

    try fix err v3

--- src/components/Ballons.tsx
+++ src/components/Ballons.tsx
@@ -1,13 +1,16 @@
 "use client";
-import Lottie from "lottie-react";
-import animationData from "@/assets/ballons.json";
+// import Lottie from "lottie-react";
+// import animationData from "@/assets/ballons.json";
 
-const LottieAnimation = (props: React.HTMLAttributes<HTMLDivElement>) => {
-  return (
-    <div {...props}>
-      <Lottie animationData={animationData} loop={true} />
-    </div>
-  );
+// const LottieAnimation = (props: React.HTMLAttributes<HTMLDivElement>) => {
+const LottieAnimation = () => {
+	// return (
+	//   <div {...props}>
+	//     <Lottie animationData={animationData} loop={true} />
+	//   </div>
+	// );
+
+	return <div>Balloons</div>;
 };
 
 export default LottieAnimation;
