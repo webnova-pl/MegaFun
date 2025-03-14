228aecf690960d16cd486e6e78bbe031a9ea82e3
Dawid Slowik <dawidslowik379@gmail.com>
2025-03-14 12:05:53 +0100

    test fix error

--- src/app/atrakcje/page.tsx
+++ src/app/atrakcje/page.tsx
@@ -3,19 +3,20 @@ import { getAttraction } from "@/lib/query";
 import { AttractionType } from "@/types";
 
 const AttractionsPage = async () => {
-	const data: AttractionType[] = await getAttraction();
+	// const data: AttractionType[] = await getAttraction();
 
-	return (
-		<main className="py-40">
-			<div className="container">
-				<div className="mt-40 grid grid-cols-3 gap-10">
-					{data.map((item, idx) => (
-						<AttractionCard key={`${item.id}-${idx}`} attraction={item} />
-					))}
-				</div>
-			</div>
-		</main>
-	);
+	// return (
+	// 	<main className="py-40">
+	// 		<div className="container">
+	// 			<div className="mt-40 grid grid-cols-3 gap-10">
+	// 				{data.map((item, idx) => (
+	// 					<AttractionCard key={`${item.id}-${idx}`} attraction={item} />
+	// 				))}
+	// 			</div>
+	// 		</div>
+	// 	</main>
+	// );
+	return <main>Attraction Page</main>;
 };
 
 export default AttractionsPage;
