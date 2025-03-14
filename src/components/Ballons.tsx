2fcecb7e85bc16afa8c6e57abd7f050a745f676a
Dawid Slowik <dawidslowik379@gmail.com>
2025-03-14 12:44:31 +0100

    try fix error with lottie

--- src/components/Ballons.tsx
+++ src/components/Ballons.tsx
@@ -1,13 +1,17 @@
 "use client";
-// import Lottie from "lottie-react";
-// import animationData from "@/assets/ballons.json";
+import { useEffect, useState } from "react";
+import Lottie from "lottie-react";
+import animationData from "@/assets/ballons.json";
 
 const LottieAnimation = (props: React.HTMLAttributes<HTMLDivElement>) => {
+	const [isClient, setIsClient] = useState(false);
+
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
+
 	return (
-		<div {...props}>
-			{/* <Lottie animationData={animationData} loop={true} /> */}
-			ballons
-		</div>
+		<div {...props}>{isClient ? <Lottie animationData={animationData} loop={true} /> : null}</div>
 	);
 };
 
--- src/sections/HeroSection/HeroSection.tsx
+++ src/sections/HeroSection/HeroSection.tsx
@@ -2,8 +2,10 @@ import { links } from "@/constants";
 import Link from "next/link";
 import { FC } from "react";
 import HeroImg from "./elements/HeroImg";
-import Ballons from "@/components/Ballons";
 import ArrowButton from "@/ui/Buttons/ArrowButton";
+import dynamic from "next/dynamic";
+
+const Ballons = dynamic(() => import("@/components/Ballons"), { ssr: false });
 
 interface IHeroSection {
 	showButtons?: boolean;
@@ -12,34 +14,45 @@ interface IHeroSection {
 
 const HeroSection: FC<IHeroSection> = ({ showButtons = true, showImg = true }) => {
 	return (
-			<section
-				className={`soft-shadow relative z-20 overflow-hidden lg:rounded-b-[5rem] rounded-b-[2rem] bg-cover bg-bottom shadow-2xl lg:mt-0`}
+		<section
+			className={`soft-shadow relative z-20 overflow-hidden rounded-b-[2rem] bg-cover bg-bottom shadow-2xl lg:mt-0 lg:rounded-b-[5rem]`}
+		>
+			{showImg ? <HeroImg bigImg={showButtons} /> : ""}
+			<div
+				className={`absolute inset-0 flex h-[85vh] w-screen flex-col items-center justify-center px-8`}
 			>
-				{showImg ? <HeroImg bigImg={showButtons} /> : ""}
-				<div
-					className={`absolute inset-0 flex h-[85vh] w-screen flex-col items-center justify-center px-8`}
+				<h1
+					data-aos="fade-up"
+					className={`text-bold z-40 text-center text-5xl font-bold text-white lg:text-7xl lg:tracking-wide`}
 				>
-					<h1 data-aos="fade-up"
-						className={`text-bold z-40 text-center text-5xl font-bold text-white lg:text-7xl lg:tracking-wide`}
-					>
-						MEGA FUN <br />{" "}
-						<span data-aos="fade-up" data-aos-delay="300" className="text-2xl lg:text-4xl"> Dmuchańce które rozkręcą każdą impreze</span>
-					</h1>
+					MEGA FUN <br />{" "}
+					<span data-aos="fade-up" data-aos-delay="300" className="text-2xl lg:text-4xl">
+						{" "}
+						Dmuchańce które rozkręcą każdą impreze
+					</span>
+				</h1>
 
-					{showButtons ? (
-						<div className="z-40 mt-8 flex items-center justify-center text-xl gap-4 text-white lg:flex-row">
-							<Link data-aos="fade-up" data-aos-delay="400" href={links.pricelist}>Galeria zdjęć</Link>
-							<Link data-aos="fade-up" data-aos-delay="600" className=" lg:ml-12 lg:mt-0" href={links.attractions}>
-								<ArrowButton>Zobacz oferte</ArrowButton>
-							</Link>
-						</div>
-					) : (
-						""
-					)}
-				</div>
-				<Ballons className="absolute bottom-0 left-0 w-full lg:w-2/5"></Ballons>
-				<Ballons className="absolute bottom-0 right-0 max-lg:hidden lg:w-2/5"></Ballons>
-			</section>
+				{showButtons ? (
+					<div className="z-40 mt-8 flex items-center justify-center gap-4 text-xl text-white lg:flex-row">
+						<Link data-aos="fade-up" data-aos-delay="400" href={links.pricelist}>
+							Galeria zdjęć
+						</Link>
+						<Link
+							data-aos="fade-up"
+							data-aos-delay="600"
+							className="lg:ml-12 lg:mt-0"
+							href={links.attractions}
+						>
+							<ArrowButton>Zobacz oferte</ArrowButton>
+						</Link>
+					</div>
+				) : (
+					""
+				)}
+			</div>
+			<Ballons className="absolute bottom-0 left-0 w-full lg:w-2/5"></Ballons>
+			<Ballons className="absolute bottom-0 right-0 max-lg:hidden lg:w-2/5"></Ballons>
+		</section>
 	);
 };
 
